ab9577ffaee7fdbf8bc9c15d8939a369f9297b79
Davide Albano <davidealbano96@gmail.com>
2026-03-25 16:22:22 +0100

    added sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  vite: {
+      plugins: [tailwindcss()]
+	},
+
+  integrations: [sanity({
+	projectId: 'bomj2fjw',
+	dataset: 'addoloratanet',
+	useCdn: false,
+	studioBasePath: '/admin'
+  }), react()]
 });
