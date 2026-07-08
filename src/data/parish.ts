13f6be59474533d54ca801c3bb304f9d947ad57f
Davide Albano <davidealbano96@gmail.com>
2026-07-08 11:40:31 +0200

    fixed theme and added icon

---
+++ src/data/parish.ts
@@ -0,0 +1,19 @@
+export const address = "V.le della Venezia Giulia 134, 00177 Roma";
+
+export const contact = {
+	phone: "06 259 3161",
+	phoneHref: "tel:+39062593161",
+	email: "smaddolorata@gmail.com",
+};
+
+export const parroco = {
+	name: "Don Antonio d'Errico",
+	email: "antonioderrico66@gmail.com",
+};
+
+export const socials = [
+	{ name: "Facebook", href: "https://facebook.com/smaddolorata" },
+	{ name: "Instagram", href: "https://instagram.com/giovani_addolorata" },
+	{ name: "YouTube", href: "https://www.youtube.com/channel/UCss8ZJSwWIh7Yj_Lz-9ImMw" },
+	{ name: "Twitter", href: "https://twitter.com/smaddolorata" },
+];
